329b9f6af4ce16808151c350c445110f0a3e4877
leijinbiao@foxmail.com <lswd7>
2022-03-30 14:26:31 +0800

    Initial commit

---
+++ model1/src/main/java/com/example/model1/Model1.java
@@ -0,0 +1,5 @@
+package com.example.model1;
+
+public class Model1 {
+    public static String model1="model1";
+}
---
+++ model2/src/main/java/com/example/model2/Model2.java
@@ -0,0 +1,5 @@
+package com.example.model2;
+
+public class Model2 {
+    public static String model2="model2";
+}
